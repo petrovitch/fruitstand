4cf2b672b82a24d7f3a966efd694f335b2703bc9
Alex Henderson <alex.henderson@pushpay.com>
2018-05-26 17:59:11 -0700

    Add support for TLS 1.2

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Owin;
+﻿using System.Net;
+using Microsoft.Owin;
 using Owin;
 [assembly: OwinStartup(typeof(Web.Startup))]
 namespace Web
@@ -7,7 +8,19 @@ namespace Web
 	{
 		public void Configuration(IAppBuilder app)
 		{
+			AllowBetterCrypto();
+
 			app.MapSignalR();
+		}
+
+		private static void AllowBetterCrypto()
+		{
+			// by default TLS 1.1 and 1.2 are not enabled
+			ServicePointManager.SecurityProtocol =
+				SecurityProtocolType.Ssl3 |
+				SecurityProtocolType.Tls |
+				SecurityProtocolType.Tls11 |
+				SecurityProtocolType.Tls12;
 		}
 	}
 }
